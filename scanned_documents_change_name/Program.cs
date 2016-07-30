fcc0b099c3166b08786a247e87ef392fdbe88577
Chris Idema <chrisidema@hotmail.com>
2016-07-30 21:17:33 +0200

    filter op bestandstypen toegevoegd

--- scanned_documents_change_name/Program.cs
+++ scanned_documents_change_name/Program.cs
@@ -36,27 +36,37 @@ namespace scanned_documents_change_name
             //save file as new name in out
 
 
-            string[] input_file_paths = Directory.GetFiles(@"in");//get list of input files
+            string[] input_file_paths = Directory.GetFiles( Directory.GetCurrentDirectory() );//get list of input files
 
-            Directory.CreateDirectory("out");//create output folder
+           // Directory.CreateDirectory("out");//create output folder
 
             foreach (string file_path_name in input_file_paths)
             {
+
+                if( 
+                    (Path.GetExtension(file_path_name) == ".pdf")||
+                    (Path.GetExtension(file_path_name) == ".png")||
+                    (Path.GetExtension(file_path_name) == ".jpg")
+                  )
+                {
+
                 string output_name;
+
 
 
                 output_name = "test" + file_path_name ;
 
-                output_name = Path.Combine("out", change_name(Path.GetFileName(file_path_name), File.GetLastWriteTime(file_path_name)));
+                output_name = Path.Combine(Path.GetDirectoryName(file_path_name), change_name(Path.GetFileName(file_path_name), File.GetLastWriteTime(file_path_name)));
 
-                File.Copy(file_path_name, output_name);
+                //File.Copy(file_path_name, output_name);
+
+                File.Move(file_path_name, output_name);
 
 
                 Console.WriteLine("Input name: " + file_path_name + " Output name: " + output_name);
 
-            
+                }
 
-      
 
             }
 
